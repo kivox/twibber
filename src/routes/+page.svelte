ef3735de1b40ac62ebd34dc3d148a6bcd14b6598
Petar Markov <petar@petarmarkov.co.uk>
2023-07-12 16:50:35 +0100

    tweeb example

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -15,13 +15,27 @@
         }
     ]}
 >
-    <div class="h-64 w-full bg-gray-950 rounded p-4">a</div>
-    <div class="h-64 w-full bg-gray-950 rounded p-4">a</div>
-    <div class="h-64 w-full bg-gray-950 rounded p-4">a</div>
-    <div class="h-64 w-full bg-gray-950 rounded p-4">a</div>
-    <div class="h-64 w-full bg-gray-950 rounded p-4">a</div>
-    <div class="h-64 w-full bg-gray-950 rounded p-4">a</div>
-    <div class="h-64 w-full bg-gray-950 rounded p-4">a</div>
-    <div class="h-64 w-full bg-gray-950 rounded p-4">a</div>
-    <div class="h-64 w-full bg-gray-950 rounded p-4">a</div>
+    <div class="flex flex-col w-full bg-gray-950 rounded">
+        <div class="flex flex-row items-center gap-4 h-18 p-4">
+            <img class="w-12 h-12 rounded-full" src="https://cdn.cms-twdigitalassets.com/content/dam/about-twitter/en/brand-toolkit/brand-download-img-1.jpg.twimg.2560.jpg" alt="Twibber Logo">
+            <div class="flex flex-col">
+                <div class="text-white">Twibber</div>
+                <div class="text-gray-400 text-xs">@twibber</div>
+            </div>
+            <div class="flex-grow"></div>
+            <div class="text-gray-400 text-xs h-full">1h</div>
+        </div>
+        <hr class="border-gray-800">
+        <div class="p-4">
+            Good Evening!
+        </div>
+        <hr class="border-gray-800">
+        <div class="w-full flex flex-row p-4">
+            <div class="w-full">Comment</div>
+            <div class="w-full">Retweeb</div>
+            <div class="w-full">Like</div>
+            <div class="w-full">Analytics</div>
+            <div class="w-full">Download</div>
+        </div>
+    </div>
 </Box>
